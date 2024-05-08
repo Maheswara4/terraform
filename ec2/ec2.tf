64887274eef33c2dbae583754c2836f4360f997d
maheswara4 <maheswara.maddireddy@gmail.com>
2024-05-08 12:16:41 +0530

    HCL

---
+++ ec2/ec2.tf
@@ -0,0 +1,34 @@
+resource "aws_instance" "db_creation" {
+    ami ="ami-090252cbe067a9e58"
+    vpc_security_group_ids =[aws_security_group.allow_ssh.id]
+    instance_type = "t2.micro"
+    tags = {
+        Name = "db_creation"
+  }
+}
+
+# resource resource_type resource_name
+resource "aws_security_group" "allow_ssh" {
+    name="allow_ssh"
+    description = "allow ssh access"
+    ingress { #inbond security group
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+    egress {  #outbond of security group
+        from_port        = 0 # 0 means all ports
+        to_port          = 0
+        protocol         = "-1"  #-1 means all protocols
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+
+    tags = {
+        Name = "allow_ssh"
+        createdBy="MaheswaraReddy"
+  }
+    
+}
+
